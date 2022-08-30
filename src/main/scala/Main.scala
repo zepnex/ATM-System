cccef802bb805e27039ae443b26aa6d864c17708
Mateo Ivankovic <mateo.ivankovic@outlook.com>
2022-08-30 23:39:51 +0200

    few checks added

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,6 +3,7 @@ import Bank.Acc
 
 import scala.io.StdIn.readLine
 import scala.runtime.Nothing$
+import scala.util.Random
 
 object Main extends App {
   val help2 = "(e)xit \n(w)ithdraw \n(d)eposit \n(t)ransfer \n(b)alance"
@@ -16,6 +17,7 @@ object Main extends App {
     currentParser(readLine()).execute()
 
   }
+
 
   trait Parser {
     def apply(x: String): Command
@@ -31,6 +33,7 @@ object Main extends App {
       case "t" => Transfer()
       case "b" => Balance()
       case _ => Failure("Unknown Command")
+      //TODO: implement Change Pin
     }
   }
 
@@ -54,29 +57,37 @@ object Main extends App {
   case class Withdraw() extends Command {
     override def execute(): Unit = {
       println("How much money do you want do withdraw?")
-      val x = readLine().toInt
-      if (x > 0)
-        if (AccountManager.user.getBalance - x >= 0)
-          AccountManager.user = AccountManager.user.withdraw(x)
-          println(s"Current balance: ${AccountManager.user.getBalance}")
+      try {
+        val x = readLine().toInt
+        if (x > 0)
+          if (AccountManager.user.getBalance - x >= 0)
+            AccountManager.user = AccountManager.user.withdraw(x)
+            println(s"Current balance: ${AccountManager.user.getBalance}")
+          else
+            Failure("not enough Balance").execute()
         else
-          Failure("not enough Balance").execute()
-      else
-        Failure("Must be positive number").execute()
-
+          Failure("Must be positive number").execute()
+      } catch {
+        case _ => Failure("Invalid arguments").execute()
+      }
     }
   }
 
   case class Deposit() extends Command {
     override def execute(): Unit = {
       println("How much money do you want do deposit?")
-      val x = readLine().toInt
-      if (x < 0)
-        AccountManager.user = AccountManager.user.deposit(x)
-        println(s"Current balance: ${AccountManager.user.getBalance}")
-      else
-        Failure("Must be positive number").execute()
+      try {
+        val x = readLine().toInt
+        if (x > 0)
+          AccountManager.user = AccountManager.user.deposit(x)
+          println(s"Current balance: ${AccountManager.user.getBalance}")
+        else
+          Failure("Must be positive number").execute()
+      } catch {
+        case _ => Failure("Invalid arguments").execute()
+      }
     }
+
   }
 
   case class Transfer() extends Command {
@@ -91,40 +102,39 @@ object Main extends App {
     override def execute(): Unit = println("Error: " + msg)
   }
 
+  case class Success(msg: String) extends Command {
+    override def execute(): Unit = println("Success: " + msg)
+  }
+
   case class Login() extends Command {
     override def execute(): Unit = {
       println("Please enter your Bank-Information's: <IBAN> <PIN>")
       val x = readLine().split(" ")
-      if (x.length == 2)
+      try {
         val acc = Bank.login(x(0), x(1))
         if (acc.isDefined)
           currentParser = AccountManager
           AccountManager.user = acc.get
           help = help2
         else Failure("Incorrect credentials").execute()
-      else
-        Failure("Invalid arguments").execute()
+      } catch {
+        case _ => Failure("Invalid arguments").execute()
+      }
     }
   }
 
   case class CreateAccount() extends Command {
     override def execute(): Unit = {
-      println("Enter PIN: 4 Digits")
-      val x = readLine()
-      try {
-        val z = x.toInt
-        if (z > 999 && z <= 9999)
-          val success: Boolean = Bank.createAccount((Bank.listOfAccount.size + 1).toString, x)
-          if (success)
-            println("Account has been created")
-            println(s"Your credentials are: ${Bank.listOfAccount.size} , $x")
-          else
-            throw new Exception()
-        else throw new IllegalStateException()
-      } catch {
-        case e: IllegalStateException => Failure("PIN must be 4 Digits!").execute()
-        case _ => Failure("Something went wrong please try again").execute()
+      var iban: Int = -1
+      while (iban == -1 || checkIBAN(iban)) {
+        iban = generateIBAN()
       }
+      val pin = generatePIN()
+      val success = Bank.createAccount(iban.toString, pin)
+      if (success)
+        Success("Account has been created")
+        println(s"Your Credentials are: $iban $pin")
+      else Failure("Something went wrong, pleas try again")
     }
   }
 
@@ -135,7 +145,7 @@ object Main extends App {
       if (x.length == 2)
         val success = Bank.removeAccount(x(0), x(1))
         if (success)
-          println("Account has been deleted")
+          Success("Account has been deleted").execute()
         else
           Failure("Incorrect credentials").execute()
       else
@@ -143,5 +153,22 @@ object Main extends App {
     }
   }
 
+  def generateIBAN(): Int = {
+    val x: Int = new Random().between(1, 9999)
+    println(x)
+    Bank.listOfAccount.map({
+      case (id, _) if (id._1.toInt != x) => x
+      case _ => -1
+    })
+    x
+  }
 
+  def generatePIN(): String = {
+    new Random().between(1000, 9999).toString
+  }
+
+  def checkIBAN(iban: Int): Boolean = {
+    Bank.listOfAccount.find(_._1._1.toInt == iban).isDefined
+  }
 }
+
